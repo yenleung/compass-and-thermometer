f471db95e11f04b4cb687de5c26756243dccbaf6
yenleung <129182069+yenleung@users.noreply.github.com>
2023-04-05 13:36:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,20 @@
+let temp = 0
+let degrees = 0
+input.onButtonPressed(Button.A, function () {
+    temp = input.temperature()
+    basic.showNumber(temp)
+})
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else if (degrees < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+})
